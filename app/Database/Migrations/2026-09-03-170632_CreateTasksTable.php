b697149c53262a5dc0b560666b090e0e7ae19c85
Gabriel A. Becher <86435253+Gabriel-Becher@users.noreply.github.com>
2026-09-03 16:41:48 -0300

    Update database migration

--- app/Database/Migrations/2026-09-03-170632_CreateTasksTable.php
+++ app/Database/Migrations/2026-09-03-170632_CreateTasksTable.php
@@ -3,16 +3,51 @@
 namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
+use CodeIgniter\Database\RawSql;
 
 class CreateTasksTable extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'auto_increment' => true,
+            ],
+            'title' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 255,
+                'null'       => false,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'status' => [
+                'type'       => 'ENUM',
+                'constraint' => ['pendente', 'em andamento', 'concluída'],
+                'default'    => 'pendente',
+                'null'       => false,
+            ],
+            'created_at' => [
+                'type'    => 'TIMESTAMP',
+                'null'    => false,
+                'default' => new RawSql('CURRENT_TIMESTAMP'),
+            ],
+            'updated_at' => [
+                'type'    => 'TIMESTAMP',
+                'null'    => false,
+                'default' => new RawSql('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tasks');
+
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('tasks', true);
     }
 }
