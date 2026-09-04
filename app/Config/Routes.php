8006de38554c78809633f3b86143f9e404d53d30
Gabriel A. Becher <86435253+Gabriel-Becher@users.noreply.github.com>
2026-09-04 15:13:50 -0300

    Updated routes to RESTful standard

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,21 +4,23 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 
+$routes->addRedirect('/', 'tasks');
+
 $routes->group('tasks', function ($routes) {
     $routes->get('/', 'TaskController::index');
     $routes->get('new', 'TaskController::create');
     $routes->post('/', 'TaskController::store');
 
-    $routes ->get('(:num)/edit', 'TaskController::edit/$1');
-    $routes->put('(:num)', 'TaskController::update/$1');
-    $routes->delete('(:num)', 'TaskController::delete/$1');
+    $routes ->get('edit/(:num)', 'TaskController::edit/$1');
+    $routes->put('/(:num)', 'TaskController::update/$1');
+    $routes->delete('/(:num)', 'TaskController::delete/$1');
 
 });
 
 $routes->group('api/tasks', function ($routes) {
     $routes->get('/', 'TaskApiController::index');
     $routes->post('/', 'TaskApiController::store');
-    $routes->get('(:num)', 'TaskApiController::show/$1');
-    $routes->put('(:num)', 'TaskApiController::update/$1');
-    $routes->delete('(:num)', 'TaskApiController::delete/$1');
+    $routes->get('/(:num)', 'TaskApiController::show/$1');
+    $routes->put('/(:num)', 'TaskApiController::update/$1');
+    $routes->delete('/(:num)', 'TaskApiController::delete/$1');
 });
